a67f748148823e0715b9b92c026892104bd2ace6
garzaro <garzar@romans-mbp.attlocal.net>
2018-04-27 10:47:57 -0700

    initial definitions for tables

---
+++ db_definitions.sql
@@ -0,0 +1,50 @@
+-- SQL table definitions for Team Pictor Group project
+
+
+
+DROP TABLE IF EXISTS `user`; 
+DROP TABLE IF EXISTS `post`; 
+DROP TABLE IF EXISTS `request`; 
+
+CREATE TABLE `user` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(255) NOT NULL, 
+    `last_name` varchar(255) NOT NULL, 
+    `email` varchar(320) NOT NULL,
+    `location` varchar(255) NOT NULL,
+    `public_key` varchar(2048) NOT NULL,
+    `json_block` json, 
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `post` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `user_id` int NOT NULL,
+    `title` varchar(500), 
+    `body` varchar(2000),
+    `date` datetime, 
+    `url` varchar(255), 
+    `post_type` varchar(255),
+    `encrypted` tinyint(1),
+    PRIMARY KEY(`id`),
+    CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) 
+    REFERENCES `user`(`id`)  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `request` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `sender_id` int NOT NULL,
+    `receiver_id` int NOT NULL,
+    `accepted_date` datetime,
+    `date` datetime, 
+    `req_accepted` tinyint(1), 
+    `blocked` tinyint(1),
+    PRIMARY KEY(`id`),
+    CONSTRAINT `request_ibfk_1` FOREIGN KEY (`sender_id`) 
+    REFERENCES `user`(`id`),  
+    CONSTRAINT `request_ibfk_2` FOREIGN KEY (`receiver_id`) 
+    REFERENCES `user`(`id`)  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
