efe2107f007584dbea8e14debb70716a2505c582
Kenny Lew <thegeophysicist@users.noreply.github.com>
2018-04-30 16:39:41 -0500

    Add encryption function

--- public/js/encryption.js
+++ public/js/encryption.js
@@ -1,9 +1,15 @@
 async function createAndStoreKeyPair() {
-  let keys = await keyGen();
+  let keyPair = await keyGen();
   let conn;
+  let email = "harrypotter@gmail.com";
   try {
     conn = await connect("PictorStore", "keys", 1);
-    await storeData(conn, "keys", keyPair);
+    let data = {
+      publicKey:  keyPair.publicKey,
+      privateKey: keyPair.privateKey,
+      email:      email
+    };
+    await storeData(conn, "keys", data);
   } catch(exception) {
     console.error(exception);
   } finally {
@@ -29,7 +35,7 @@ function connect(dbName, objectStoreName, version) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open(dbName, version);   request.onupgradeneeded = () => {
       let db = request.result;
-      let store = db.createObjectStore(objectStoreName, {autoIncrement:true});
+      let store = db.createObjectStore(objectStoreName, {keyPath: "email"});
     };
     request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
@@ -45,4 +51,62 @@ function storeData(conn, objectStoreName, data) {
     request.onsuccess = () => resolve(request.result);
     request.error = () => reject(request.error);
   });
+}
+
+function getData(conn, objectStoreName, value) {
+  return new Promise((resolve, reject) => {
+    const tx = conn.transaction(objectStoreName, "readonly");
+    const store = tx.objectStore(objectStoreName);
+    const request = store.get(value);
+    request.onsuccess = () => resolve(request.result);
+    request.error = () => reject(request.error);
+  });
+}
+
+
+testData = {
+  id: 1,
+  name:     'Harry Potter',
+  location: {
+    city:   'New York City',
+    state:  'New York'
+  }
+}
+
+async function encryptData(data) {
+  let conn;
+  let key;
+  let email = "harrypotter@gmail.com";
+  try {
+    conn = await connect("PictorStore", "keys", 1);
+    let key = await getData(conn, "keys", email);
+    let uint8array = new TextEncoder("utf-8").encode(JSON.stringify(data));
+    let encryptedData = await encrypt(key.privateKey, uint8array);
+    console.log(encryptedData);
+  } catch(exception) {
+    console.error(exception);
+  } finally {
+    if(conn)
+      conn.close();
+  }
+}
+
+function encrypt(key, data) {
+  return window.crypto.subtle.encrypt(
+    {
+      name: "RSA-OAEP",
+    },
+    key,
+    data
+  )
+}
+
+function decrypt(key, data) {
+  return window.crypto.subtle.decrypt(
+    {
+      name: "RSA-OAEP",
+    },
+    key,
+    data
+  )
 }
