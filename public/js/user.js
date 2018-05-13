18033487678f7cd465ad851dff8bfc8d4edf11f0
Kenny Lew <thegeophysicist@users.noreply.github.com>
2018-05-13 18:06:19 -0500

    Change edit to dynamically redirect to user's profile

--- public/js/user.js
+++ public/js/user.js
@@ -22,7 +22,7 @@ async function handleEditLocationSubmit(event) {
     // Encrypt payload with server public key
     // POST data from form
     postDataToUrl("/api/user/update", payload);
-    window.location.pathname = "/user/5432";
+    window.location.pathname = `${window.location.pathname}`;
 }
 
 async function handleEditAboutSubmit(event) {
@@ -52,7 +52,7 @@ async function handleEditAboutSubmit(event) {
     // Encrypt payload with server public key
     // POST data from form
     postDataToUrl("/api/user/update", payload);
-    window.location.pathname = "/user/5432";
+    // window.location.pathname = "/user/5432";
 }
 
 function getCurrentLocationData() {
