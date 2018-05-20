b0726a596eb712337a035cee0c52b8e7634238ea
youngroz <garzar@oregonstate.edu>
2018-05-19 22:06:31 -0700

    ReFix hang when invalid login info is submitted

--- routes/app/signin.js
+++ routes/app/signin.js
@@ -10,28 +10,25 @@ router.post('/signin', async function(req, res) {
   const { email, password } = req.body;
 
   const result = await User.findPass({email, password});
-  const hash = result[0].password;
-  //console.log(hash);
-  let compareResult = false;
-
+  //console.log(result);
   try {
-    // check that passwords match
-    const cRes = await bcrypt.compare(password, hash).then(async function(res){
-      //console.log(res);
-      //console.log(result[0].id)
-      return res;
-    });
-
-    try {
-      // if they match
-      if(cRes){
-        // log user into passport
-        req.login(result[0].id, function(err){ if(err) throw err;});
+    if(result.length>0){
+      const hash = result[0].password;
+      //console.log(hash);
+      // check that passwords match
+      const cRes = await bcrypt.compare(password, hash).then(async function(res){
+        return res;
+      });
+      try {
+        // if they match
+        if(cRes){
+          // log user into passport
+          req.login(result[0].id, function(err){ if(err) throw err;});
+        }
+      }catch (error) {
+        console.error(error);
       }
-    }catch (error) {
-      console.error(error);
     }
-    //res.redirect('/feed');
     res.json([]);
   } catch (error) {
     console.error(error);
